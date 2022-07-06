5bb8b67e31803f0bc417cb4b5ef4f16b75213152
emlegweak <emilythearmstrong@gmail.com>
2022-07-05 21:59:15 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+//require dependencies
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+
+//declare variables
+let db, 
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'star-wars-quotes',
+    collection 
+
+//establish database connection
+MongoClient.connect(dbConnectionString)
+//set initial parameters for db connection
+    .then(client => {
+        console.log('Connected to Database! Isn\'t that swell.');
+        db = client.db(dbName)
+        collection = db.collection('quotes')
+    })
+
+//middleware
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+//create PORT
+app.listen(process.env.PORT || PORT, () =>{
+    console.log(`Server is running on port ${process.env.PORT}. Better go catch it...`)
+})
